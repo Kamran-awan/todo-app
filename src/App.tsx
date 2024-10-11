24f129ad8fd0de676a7a8219a5d30a7f94dca568
Kamran-awan <kamranawan343@gmail.com>
2024-10-12 00:23:27 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TodoApp from "./component/Todo";
+import "./App.css"
+
+
+
+export default function App(){
+    return (
+        <div className="App">
+            <h1>My Todo List</h1>
+            <TodoApp/>
+        </div>
+    )
+}
---
+++ src/component/Todo.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+interface Todo{
+    id:number;
+    task:string;
+}  
+
+const TodoApp =() =>{
+    const [task, setTest] =useState <string> ("");
+    const [todos, setTodos] = useState <Todo[]> ([]);
+
+
+const addTask = () => {
+    if (task.trim() === "") return;
+    const newTask = {id: Date.now(), task };
+    setTodos([...todos,newTask]);
+    setTest("")
+};
+const deleteTask  =(id:number) =>{
+    setTodos(todos.filter(todo=>todo.id !==id));
+};
+
+return (
+    <div className="todo-container">
+        <div className="input-section">
+            <input
+            type="text"
+            value={task}
+            onChange={(e)=> setTest(e.target.value)}
+            placeholder="Enter Your Task"
+            />
+            <button onClick={addTask}
+            disabled= {!task}
+            className={task ? "active" : ""}> 
+            Add Task
+            </button>
+
+        </div>
+
+        <ul>
+            {todos.map((todo)=>(
+                <li key={todo.id}>
+                    {todo.task}
+                    <button onClick={() => deleteTask (todo.id)}>Delete</button>
+                </li>
+
+            ))}
+        </ul>
+
+
+        </div>
+);
+     
+};
+export default TodoApp;
